4445a79172ea872e2cade991dd642a3c9c409c1e
Rohit Ranjan <rohitranjan0839@gmail.com>
2020-12-22 23:49:47 +0530

    Create Initialization.cpp

---
+++ Basic/Initialization.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using std::cout;
+using std::cin;
+
+int main() 
+{
+  int a = 2;            // copy initialization
+  auto b = 4;
+
+  int x, y, z = 5; 
+  x = y = 10;
+  
+  int l (1);             // direct initialization
+  auto m (1);
+  
+  int n {4};             // uniform initialization 
+  auto p {4};
+}
